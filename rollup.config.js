87967a379b404a0dd07d6a6c0c9b0442b894e1a0
Adrian Heine <mail@adrianheine.de>
2016-09-27 09:24:26 +0200

    Add buble to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import buble from 'rollup-plugin-buble';
+
 export default {
   banner: `\/\/ CodeMirror, copyright (c) by Marijn Haverbeke and others
 \/\/ Distributed under an MIT license: http:\/\/codemirror.net/LICENSE
@@ -10,5 +12,6 @@ export default {
   entry: "src/codemirror.js",
   format: "umd",
   dest: "lib/codemirror.js",
-  moduleName: "CodeMirror"
+  moduleName: "CodeMirror",
+  plugins: [ buble() ]
 };
