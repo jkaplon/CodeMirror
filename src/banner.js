a0d1927f32a7570361cc3942ee0813b36817c503
Adrian Heine <mail@adrianheine.de>
2016-09-27 09:23:50 +0200

    Move to rollup.config.js

--- src/banner.js
+++
@@ -1,8 +0,0 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: http://codemirror.net/LICENSE
-
-// This is CodeMirror (http://codemirror.net), a code editor
-// implemented in JavaScript on top of the browser's DOM.
-//
-// You can find some technical background for some of the code below
-// at http://marijnhaverbeke.nl/blog/#cm-internals .
